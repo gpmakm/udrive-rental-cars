0c60c6579035e17d07a92011edf1371814a51b82
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-04-07 20:46:38 +0530

    Updated a value and added new member

--- dictionarymethods.py
+++ dictionarymethods.py
@@ -12,3 +12,10 @@ print(f"Name of the college is {college['name']}")
 print(f"Principal of the college is {college['principal']}")
 print(f"Departments in the college are {college['dept']}")
 print(f"I am a student of {college['dept'][0]} department")
+
+# trying to update a value using key
+college["name"]="Government Engineering College Aurangabad"
+print(college['name'])
+# trying to add a new key value pair
+college["location"]="Rafiganj, Aurangabad"
+print(f"Location is {college['location']}")
