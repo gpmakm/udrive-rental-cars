7a56ad2298cdeeeb5e84d7bf5da1d1d912059dde
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-05-22 18:50:30 +0530

    Added a new module prototype

---
+++ NextGen/filterer.py
@@ -0,0 +1,9 @@
+def filterEvenNums(l):
+    return [x for x in l if x % 2==0]
+
+def filterOddNums(l):
+    return [m for m in l if m%2!=0]
+
+def filterGreathan(l,num):
+    return [n for n in l if n> num]
+
--- fileio.py
+++ fileio.py
@@ -1,7 +1,10 @@
-f=open("io.py","r")
-fbin=open("io.py","+br")
-data=fbin.read()
+#f=open("io.py","r")
+with open("io.py") as f:
+    data=f.readline()
+    d=chr(data)
+    print(d)
+
 f.close()
-f2=open("Writingsample.txt","+bw")
-f2.write(data)
+f2=open("Writingsample.txt","w")
+f2.write(bin(ascii(set(data))))
 f2.close()
