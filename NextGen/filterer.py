826be0c5cbf42860e44351ed5189225ce6d4554f
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-05-22 18:52:23 +0530

    Changed return type to tuple

--- NextGen/filterer.py
+++ NextGen/filterer.py
@@ -1,9 +1,11 @@
 def filterEvenNums(l):
-    return [x for x in l if x % 2==0]
+    return (x for x in l if x % 2==0)
 
 def filterOddNums(l):
-    return [m for m in l if m%2!=0]
+    return (m for m in l if m%2!=0)
 
 def filterGreathan(l,num):
-    return [n for n in l if n> num]
+    return (n for n in l if n> num)
 
+def filterLessThan(l,num):
+    return (x for x in l if x<num)
