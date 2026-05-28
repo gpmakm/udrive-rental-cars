e85f4e5fc15bf06f484b89296f9f6981e397a27b
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-05-28 20:19:11 +0530

    Added module to check for student's branch

---
+++ NextGen/branchchecker.py
@@ -0,0 +1,8 @@
+def branch_checker(branchcode,branchname):
+    n=input("Enter your regno: ")
+    regno=tuple(n)
+    code=tuple(branchcode)
+    if regno[2] == code[0]:
+        if regno[3] == code[1]:
+            if regno[4] == code[2]:
+                print(f"You are a student of {branchname}")
